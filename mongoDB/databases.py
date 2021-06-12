c8c8903fdd93a6c06a8f46bd0380c834da926db2
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-06-12 23:57:56 +0530

    Fix : Instances

--- mongoDB/databases.py
+++ mongoDB/databases.py
@@ -3,19 +3,22 @@ from pymongo import MongoClient
 client = MongoClient()
 client = MongoClient('mongodb://localhost:27017/')
 
-# Get a database
-db = client['<DATABASE NAME>']
+# Get a database from name
+db = client['<DATABASE_NAME>']
+
+# Get a database from instance
+db = client.some_db
 
 # List Database names
 db_names = client.list_database_names()
 print(db_names)
 
 # Get the database named in the MongoDB connection URI.
-client = MongoClient('mongodb://host/my_database')
+client = MongoClient('mongodb://host/<DATABASE_NAME>')
 db = client.get_default_database()
 
 # Get a cursor over the databases of the connected server.
 client.list_databases(session = None)
 
 # Drop a database.
-client.drop_database(name_or_database = 'my_database')
+client.drop_database(name_or_database = '<DATABASE_NAME>')
