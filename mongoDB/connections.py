9e5e511bef7f3e070b902ed4a9b516de49cfd586
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-06-12 23:57:15 +0530

    Feat : Percent-Escaping

--- mongoDB/connections.py
+++ mongoDB/connections.py
@@ -1,19 +1,25 @@
 # Install pymongo using 'pip install pymongo'
 
+import urllib
+
 from pymongo import MongoClient
 client = MongoClient()
 
-### Connect to localhost :
-## Format :
+# Connect to localhost :
+# Format :
 # client = MongoClient('<host>', port_number)
 client = MongoClient('localhost', 27017)
 #  or use the entire URL
 # client = MongoClient('mongodb://<host>:<portnum>/')
 client = MongoClient('mongodb://localhost:27017/')
 
-### Connect to Atlas :
+# Connect via URI :
 # Use pip3 install pymongo[tls] for connecting to atlas
-## Format :  
+# Format :  
 # client = pymongo.MongoClient(<Atlas connection string>)
 client = MongoClient('mongodb+srv://admin:<password>@cluster0-pm5vp.mongodb.net/test?retryWrites=true&w=majority')
 
+# Percent-Escaping Username and Password
+username = "Atharva"
+password = r"pass/with\alltpyes`of-chars"
+client = MongoClient('mongodb://%s:%s@127.0.0.1' % (urllib.parse.quote_plus(username), urllib.parse.quote_plus(password)))
