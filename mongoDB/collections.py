89031ecd4b90c4f3bb4cf54749431deceb86f17e
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-06-13 17:49:01 +0530

    Feat : Mongo Collections

---
+++ mongoDB/collections.py
@@ -0,0 +1,19 @@
+from pymongo.collation import Collation
+from pymongo import MongoClient
+
+client = MongoClient()
+client = MongoClient('mongodb://localhost:27017/')
+db = client['<DATABASE NAME>']
+
+# Get a collection from name
+collection = db['<COLLECTION_NAME>']
+
+# Get a collection from instance
+collection = db.some_collection
+
+# List collection names
+collection_names = client.list_collection_names()
+
+# Create a new collection
+# create a new collection called contacts and assign a default collation with the fr_CA locale
+collection = db.create_collection('contacts',collation=Collation(locale='fr_CA'))
