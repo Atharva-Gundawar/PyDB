f5a8b455fc70e6bebe1e26c211193054f2e71635
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-06-14 17:19:20 +0530

    Feat : autopep8

--- mongoDB/queryOperators.py
+++ mongoDB/queryOperators.py
@@ -1,3 +1,4 @@
+import datetime
 from pymongo.collation import Collation
 from pymongo import MongoClient
 
@@ -8,14 +9,14 @@ db = client['<DATABASE NAME>']
 # Get a collection from instance
 collection = db.some_collection
 
-# Some data 
-data = {  'name' : "Atharva Gundawar" , 
-          'age' : 100,
-          'gender' : "Male",
-          'greetings': ['hello','hello again','hello once more'] # Array        
-       }
+# Some data
+data = {'name': "Atharva Gundawar",
+        'age': 100,
+        'gender': "Male",
+        'greetings': ['hello', 'hello again', 'hello once more']
+        }
 
-#Insert one data/document
+# Insert one data/document
 insert_result = collection.insert_one(data)
 
 # Confirm that insert is successful
@@ -28,29 +29,30 @@ print(insert_result.inserted_id)
 print(list(collection.find()))
 
 # Find the inserted document using the objectID
-print(list(collection.find( {'_id' : insert_result.inserted_id })))  
+print(list(collection.find({'_id': insert_result.inserted_id})))
 
 # find, can use one key or more
-list(collection.find( {'name' : "Atharva" }))                    
+list(collection.find({'name': "Atharva Gundawar"}))
 
 n = 5
 
 # gets first n documents matching the given query
-list(collection.find().limit(n))                             
+list(collection.find().limit(n))
 
 # gets all documents skipping first n documents
-list(collection.find().skip(n))                              
+list(collection.find().skip(n))
 
 # Update an existing document
-update_result = collection.update_one( 
-    {'name' : "Atharva"}, 
-    {'$set' : { 'age' : 50 }}
-    ) 
+update_result = collection.update_one(
+    {'name': "Atharva Gundawar"},
+    {'$set': {'age': 50}}
+)
 # or
-collection.find_one_and_update( {'name' : "Atharva"}, {'$set' : { 'age' : 50 }})
+collection.find_one_and_update({'name': "Atharva Gundawar"}, {'$set': {'age': 50}})
 
 # Insert a new document with update, will avoid to crash during insert if document already exist
-insert_result = collection.update_one( {'name' : 'Atharva'}, {'$set' : { 'age' : 50 }}, upsert= True)
+insert_result = collection.update_one(
+    {'name': 'Atharva'}, {'$set': {'age': 50}}, upsert=True)
 
 # show update results
 print(update_result.raw_result)
